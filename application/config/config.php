88f7df23a96348c3af56e09fb5af4cb763667e9f
Geraldine Agusta <geraldine.agusta@kompas.com>
2019-11-19 17:15:36 +0700

    change config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://ep.local/';
+$config['base_url'] = 'https://esldev.kgmedia.id/ep/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://ep.local/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
