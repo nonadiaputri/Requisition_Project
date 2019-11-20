2fa0d0190791483520e70dbcb8c5193a9ad34eb0
aha <andi.hendradi@kompas.com>
2019-11-20 10:24:23 +0700

    route crud/master

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'login';
 $route['logout'] = 'login/logout';
-$route['master'] = 'crud/master';
+$route['master/(:any)'] = 'crud/master';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
