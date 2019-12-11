820d3d2da62f99afafc92ef68d15916a9e502563
Geraldine Agusta <geraldine.agusta@kompas.com>
2019-12-11 09:51:24 +0700

    adding landing page for handling error restriction area HRIS module

--- application/modules/hr/views/v_error_hris.php
+++ application/modules/hr/views/v_error_hris.php
@@ -44,7 +44,7 @@
               <div class="alert alert-danger alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                 <h4><i class="icon fa fa-ban"></i> Access Forbidden!</h4>
-                Your Personnel Info is not registered! Please contact HR to
+                Your Personnel Info is not registered! Please contact HR to resolve the problem
               </div>
     
    
