4dbe8d4fcf8f289797e91ca8368cc146462bd9b9
Geraldine Agusta <geraldine.agusta@kompas.com>
2019-12-24 10:50:40 +0700

    Bug fixing routes dashboard

--- application/modules/dashboard/views/v_dashboard_staf.php
+++ application/modules/dashboard/views/v_dashboard_staf.php
@@ -46,8 +46,7 @@
         <small>Version 2.0</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
       </ol>
     </section>
 
