cf79ff9515bd8576f4ee2325ad3329bb37cdae43
Geraldine Agusta <geraldine.agusta@kompas.com>
2020-01-15 17:28:26 +0700

    adding function cURL to get API

--- application/modules/register/controllers/Register.php
+++ application/modules/register/controllers/Register.php
@@ -7,8 +7,38 @@ class Register extends CI_Controller{
         parent::__construct();
     }
 
-    public function index(){
+    public function get_subunit($id){
+        //$id = $_REQUEST['id'];
+        $api_url = "http://kics.kompas.com/api/subunit?id=".$id;
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+				CURLOPT_URL => $api_url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_HEADER => true,
+				CURLOPT_HTTPHEADER => array(
+					"Accept: application/json"
+				),
+			));
+
+			$response = curl_exec($curl);
+			$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+			$body = substr($response, $header_size);
+			$err = curl_error($curl);
+			curl_close($curl);
+
+			
+		
+			
+            //$body = json_decode($body, true);
+            echo $body;
         
+            
     }
 
 }
