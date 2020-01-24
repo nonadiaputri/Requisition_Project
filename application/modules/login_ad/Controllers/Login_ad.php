5c72336fdbefe057b734cacd3eaf1ad44ccb5b13
Geraldine Agusta <geraldine.agusta@kompas.com>
2020-01-24 09:49:24 +0700

    Bug Fixing login AD portal

--- application/modules/login_ad/Controllers/Login_ad.php
+++ application/modules/login_ad/Controllers/Login_ad.php
@@ -5,6 +5,7 @@ class Login_ad extends CI_Controller
 {
     public function __construct() {
         parent::__construct();
+        $this->load->helper('url');
         //$this->load->library("Aauth");
     }
 
