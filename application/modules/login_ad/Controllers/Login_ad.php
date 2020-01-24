12503f038d640df7cd93f96e62add7c07660cfa8
Geraldine Agusta <geraldine.agusta@kompas.com>
2020-01-24 09:36:29 +0700

    Bug Fixing login AD portal

--- application/modules/login_ad/Controllers/Login_ad.php
+++ application/modules/login_ad/Controllers/Login_ad.php
@@ -3,6 +3,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login_ad extends CI_Controller
 {
+    public function __construct() {
+        parent::__construct();
+        //$this->load->library("Aauth");
+    }
 
     public function index()
     {
