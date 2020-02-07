4f652674e26ac54cea20b2ff39eca23cb2da5929
Nadia Kusuma Putri <nadia.putri@kompas.com>
2020-02-07 11:43:41 +0700

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://esldev.kgmedia.id/ep/';
+$config['base_url'] = 'http://ep.local/';
+
+
 
 /*
 |--------------------------------------------------------------------------
